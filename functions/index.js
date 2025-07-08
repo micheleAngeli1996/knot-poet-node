f9497b3115ca25c951ab6f68c6ad0324bb41cfc9
Michele Angeli <michele.angeli@gullivernet.com>
2025-07-08 09:38:35 +0200

    try not await transporter.sendMail

--- functions/index.js
+++ functions/index.js
@@ -8,13 +8,13 @@
  */
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const { setGlobalOptions } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/v2/https");
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
 
 // The Firebase Admin SDK to access Firestore.
-const { initializeApp } = require("firebase-admin/app");
+const {initializeApp} = require("firebase-admin/app");
 
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({maxInstances: 10});
 initializeApp();
 
 // Importa le librerie necessarie
@@ -37,20 +37,20 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-app.post("/sendMail", async (req, res) => {
-    const { to, subject, text } = req.body;
+app.post("/sendMail", (req, res) => {
+    const {to, subject, text} = req.body;
 
     try {
-        await transporter.sendMail({
+        transporter.sendMail({
             from: `"Knot Poet - Official Website" <${process.env.EMAIL_USER}>`,
             to,
             subject,
             text,
         });
-        res.status(200).json({ message: "Email inviata con successo!" });
+        res.status(200).json({message: "Email inviata con successo!"});
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Errore nell'invio dell'email" });
+        res.status(500).json({error: "Errore nell'invio dell'email"});
     }
 });
 
